4858b103540ebaa8de852033486a7d43d467816e
Daniel <rowedanielp@gmail.com>
2024-09-24 22:32:34 -0700

    feat: first full implementation of dafsa

--- readerdeleter/cpp/dafsa.cpp
+++ readerdeleter/cpp/dafsa.cpp
@@ -1,4 +1,10 @@
+/**
+ * Implements a DAFSA
+ * based on this paper: https://aclanthology.org/J00-1002.pdf
+ */
+
 #include <iostream>
+#include <list>
 #include <map>
 #include <pybind11/pybind11.h>
 #include <string>
@@ -6,24 +12,119 @@
 class DAFSAnode {
 public:
   bool terminal = false;
+  // TODO: change from using std::map to something more suited
+  // (see
+  // https://martin.ankerl.com/2022/08/27/hashmap-bench-01/#access--find-benchmarks)
+  // Perhaps the right way is simply an array of pointers length of alphabet?
   std::map<char, DAFSAnode *> children;
 };
 
 class DAFSA {
-  DAFSAnode *root = new DAFSAnode();
-  std::string previous_word = "";
 
 private:
-  int get_prefix(std::string word) {
-    /** Get common prefix between word and previous word **/
+  std::string previous_word = "";
+  std::list<DAFSAnode *> rep_reg_cache = {};
+  DAFSAnode *root = new DAFSAnode();
+
+  std::string get_prefix(std::string word) {
+    /** Get common prefix between word and previous word
+     *
+     * Note: this is slightly different than the epynomous function from the
+     * paper. Its behavior should be equivalent, though (I hope).
+     */
     int prefix;
     for (prefix = 0; prefix < word.length(); ++prefix) {
       if (word[prefix] != previous_word[prefix])
         break;
     }
-    return prefix;
+    return word.substr(0, prefix);
   }
 
+  DAFSAnode *traverse(DAFSAnode *state, std::string word) {
+    /** This is equivalent to delta* in the paper.
+     * It is used here as a helper function
+     */
+    if (word.length() == 0)
+      return state;
+
+    auto c = word[0];
+    if (state->children.find(c) == state->children.end()) {
+      return NULL;
+    }
+
+    return traverse(state->children[c], word.substr(1));
+  }
+
+  void add_suffix(DAFSAnode *state, std::string word) {
+    // naively add the word, as if in a trie
+    auto node = state;
+    for (auto c : word) {
+      if (node->children.find(c) == node->children.end()) {
+        // character not found
+        auto newNode = new DAFSAnode();
+        node->children[c] = newNode;
+        node = newNode;
+        std::cout << "creating new node: " << c << "(" << node << ")"
+                  << std::endl;
+      } else {
+        // character found
+        node = node->children[c];
+      }
+    }
+    node->terminal = true;
+  }
+
+  char last_child_key(DAFSAnode *state) {
+    return state->children.rbegin()->first;
+  }
+
+  bool equiv(DAFSAnode *a, DAFSAnode *b) {
+    std::cout << "testing equivalence of " << a << " and " << b << std::endl;
+    if (a->terminal != b->terminal) {
+      std::cout << "don't agree on terminality :(  a->terminal: " << a->terminal
+                << ", and b->terminal: " << b->terminal << std::endl;
+      return false;
+    }
+
+    // transitions are the same
+    for (auto t = a->children.begin(); t != a->children.end(); ++t) {
+      if (b->children.find(t->first) == b->children.end() ||
+          b->children[t->first] != t->second) {
+        std::cout << "no agreement on " << t->first << std::endl;
+        return false;
+      }
+    }
+    for (auto t = b->children.begin(); t != b->children.end(); ++t) {
+      if (a->children.find(t->first) == a->children.end() ||
+          a->children[t->first] != t->second) {
+        std::cout << "no agreement on " << t->first << std::endl;
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  void replace_or_register(DAFSAnode *state) {
+    auto child_key = last_child_key(state);
+    auto child = state->children[child_key];
+
+    if (!child->children.empty())
+      replace_or_register(child);
+
+    for (auto q : rep_reg_cache) {
+      if (equiv(q, child)) {
+        std::cout << "Redundant node found for child '" << child_key
+                  << "'. Deleting" << std::endl;
+        state->children[child_key] = q;
+        delete child;
+        return;
+      }
+    }
+
+    // no equivalent state exists
+    rep_reg_cache.push_back(child);
+  }
 
 public:
   void add_word(std::string word) {
@@ -34,42 +135,40 @@ public:
       std::cout << "attempted to add words out of order" << std::endl;
     }
 
+    std::cout << "Getting prefix: ";
     auto prefix = get_prefix(word);
+    std::cout << prefix << std::endl;
 
-    // naively add the word, as if in a trie
-    auto node = root;
-    for (auto c : word) {
-      if (node->children.find(c) == node->children.end()) {
-        // character not found
-        auto newNode = new DAFSAnode();
-        node->children[c] = newNode;
-        node = newNode;
-        std::cout << "creating new node: " << c << std::endl;
-      } else {
-        // character found
-        node = node->children[c];
-      }
+    std::cout << "Getting last state: ";
+    auto last_state = traverse(root, prefix);
+    std::cout << last_state << std::endl;
+
+    std::cout << "Getting current suffix: ";
+    auto current_suffix = word.substr(prefix.length());
+    std::cout << current_suffix << std::endl;
+
+    if (!last_state->children.empty()) {
+      std::cout << "replacing or registering " << std::endl;
+      replace_or_register(last_state);
     }
-    node->terminal = true;
+
+    std::cout << "Adding suffix" << std::endl;
+    add_suffix(last_state, current_suffix);
+
     previous_word = word;
+
+    std::cout << "Register presently looks like: [";
+    for (auto it : rep_reg_cache) {
+      std::cout << it << ", ";
+    }
+    std::cout << "]" << std::endl;
   }
 
+  void finish() { replace_or_register(root); }
+
   bool is_word(std::string word) {
-    auto node = root;
-    for (auto c : word) {
-      if (node->children.find(c) == node->children.end()) {
-        std::cout << "letter not found: " << c << std::endl;
-        return false;
-      }
-      std::cout << "letter found: " << c << ", and node terminality is "
-                << node->terminal << std::endl;
-      node = node->children[c];
-    }
-    std::cout << "node terminality is " << node->terminal << std::endl;
-    if (node->terminal) {
-      return true;
-    }
-    return false;
+    auto node = traverse(root, word);
+    return node != NULL && node->terminal;
   }
 };
 
@@ -83,5 +182,6 @@ PYBIND11_MODULE(dafsa, m) {
   py::class_<DAFSA>(m, "DAFSA")
       .def(py::init<>())
       .def("add_word", &DAFSA::add_word)
-      .def("is_word", &DAFSA::is_word);
+      .def("is_word", &DAFSA::is_word)
+      .def("finish", &DAFSA::finish);
 }
--- test/test_dafsa.py
+++ test/test_dafsa.py
@@ -1,26 +1,28 @@
+import random
+
 from readerdeleter.build.dafsa import DAFSA
 
 
-def test_dafsa_positive():
+def test_positive():
     dafsa = DAFSA()
     dafsa.add_word("tests")
     assert dafsa.is_word("tests")
 
 
-def test_dafsa_negative():
+def test_negative():
     dafsa = DAFSA()
     dafsa.add_word("tests")
     assert not dafsa.is_word("foo")
 
 
-def test_dafsa_partial():
+def test_partial():
     dafsa = DAFSA()
     dafsa.add_word("test")
     assert not dafsa.is_word("tests")
     assert not dafsa.is_word("tes")
 
 
-def test_dafsa_multiple():
+def test_multiple():
     dafsa = DAFSA()
     print("adding words")
     dafsa.add_word("test")
@@ -33,3 +35,29 @@ def test_dafsa_multiple():
     assert dafsa.is_word("tests")
     assert dafsa.is_word("testes")
     assert dafsa.is_word("xenomorph")
+
+
+def test_random():
+    dafsa = DAFSA()
+
+    alphabet = "abcd"
+
+    words = [
+        "".join(random.choice(alphabet) for _ in range(random.randint(2, 10)))
+        for _ in range(100)
+    ]
+    words.sort()
+
+    nonwords = [
+        "".join(random.choice(alphabet) for _ in range(random.randint(2, 10)))
+        for _ in range(100)
+    ]
+    nonwords = [word for word in nonwords if word not in words]
+
+    for word in words:
+        dafsa.add_word(word)
+
+    for word in words:
+        assert dafsa.is_word(word)
+    for word in nonwords:
+        assert not dafsa.is_word(word)
