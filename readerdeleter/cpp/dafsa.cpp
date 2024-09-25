e231b025c475b382f44433685a559aa4ca076df3
Daniel <rowedanielp@gmail.com>
2024-09-24 19:58:45 -0700

    style: Add docstrings and clang-format dafsa

--- readerdeleter/cpp/dafsa.cpp
+++ readerdeleter/cpp/dafsa.cpp
@@ -1,84 +1,87 @@
-#include <string>
-#include <pybind11/pybind11.h>
-#include <map>
 #include <iostream>
+#include <map>
+#include <pybind11/pybind11.h>
+#include <string>
 
 class DAFSAnode {
-    public:
-        bool terminal = false;
-        std::map<char, DAFSAnode*> children;
+public:
+  bool terminal = false;
+  std::map<char, DAFSAnode *> children;
 };
 
 class DAFSA {
-    DAFSAnode * root = new DAFSAnode();
-    std::string previous_word = "";
+  DAFSAnode *root = new DAFSAnode();
+  std::string previous_word = "";
 
-    private:
-        int get_prefix(std::string word) {
-            int prefix;
-            for(prefix=0; prefix<word.length(); ++prefix) {
-                if(word[prefix] != previous_word[prefix])
-                    break;
-            }
-            return prefix;
-        }
+private:
+  int get_prefix(std::string word) {
+    /** Get common prefix between word and previous word **/
+    int prefix;
+    for (prefix = 0; prefix < word.length(); ++prefix) {
+      if (word[prefix] != previous_word[prefix])
+        break;
+    }
+    return prefix;
+  }
 
-    public:
-        void add_word(std::string word) {
-            // assume words are added in order
-            if(word < previous_word) {
-                std::cout << "attempted to add words out of order" << std::endl;
-            }
 
-            int prefix = get_prefix(word);
+public:
+  void add_word(std::string word) {
+    /** Add word to the DAFSA structure **/
 
-            // naively add the word, as if in a trie
-            auto node = root;
-            for(auto c : word) {
-                if(node->children.find(c) == node->children.end()) {
-                    // character not found
-                    auto newNode =  new DAFSAnode();
-                    node->children[c] = newNode;
-                    node = newNode;
-                    std::cout << "creating new node: " << c << std::endl;
-                } else {
-                    // character found
-                    node = node->children[c];
-                }
-            }
-            node->terminal = true;
-            previous_word = word;
-        }
+    // assume words are added in order
+    if (word < previous_word) {
+      std::cout << "attempted to add words out of order" << std::endl;
+    }
 
-        bool is_word(std::string word) {
-            auto node = root;
-            for(auto c : word) {
-                if(node->children.find(c) == node->children.end()) {
-                    std::cout << "letter not found: " << c << std::endl;
-                    return false;
-                }
-                std::cout << "letter found: " << c << ", and node terminality is " << node->terminal << std::endl;
-                node = node->children[c];
-            }
-            std::cout << "node terminality is " << node->terminal << std::endl;
-            if(node->terminal) {
-                return true;
-            }
-            return false;
-        }
+    auto prefix = get_prefix(word);
+
+    // naively add the word, as if in a trie
+    auto node = root;
+    for (auto c : word) {
+      if (node->children.find(c) == node->children.end()) {
+        // character not found
+        auto newNode = new DAFSAnode();
+        node->children[c] = newNode;
+        node = newNode;
+        std::cout << "creating new node: " << c << std::endl;
+      } else {
+        // character found
+        node = node->children[c];
+      }
+    }
+    node->terminal = true;
+    previous_word = word;
+  }
+
+  bool is_word(std::string word) {
+    auto node = root;
+    for (auto c : word) {
+      if (node->children.find(c) == node->children.end()) {
+        std::cout << "letter not found: " << c << std::endl;
+        return false;
+      }
+      std::cout << "letter found: " << c << ", and node terminality is "
+                << node->terminal << std::endl;
+      node = node->children[c];
+    }
+    std::cout << "node terminality is " << node->terminal << std::endl;
+    if (node->terminal) {
+      return true;
+    }
+    return false;
+  }
 };
 
-int add(int a, int b) {return a+b;}
-
+int add(int a, int b) { return a + b; }
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(dafsa, m) {
-    m.doc() = "Implementation of a DAFSA in C++"; // Optional module docstring
-                                                 //
-    py::class_<DAFSA>(m, "DAFSA")
-        .def(py::init<>())
-        .def("add_word", &DAFSA::add_word)
-        .def("is_word", &DAFSA::is_word);
-
+  m.doc() = "Implementation of a DAFSA in C++"; // Optional module docstring
+                                                //
+  py::class_<DAFSA>(m, "DAFSA")
+      .def(py::init<>())
+      .def("add_word", &DAFSA::add_word)
+      .def("is_word", &DAFSA::is_word);
 }
