596e0ca7f982804bc0f92567c4fa0071319f21a0
Daniel <rowedanielp@gmail.com>
2024-11-05 20:02:30 -0800

    feat: create sandbox code

---
+++ dafsa_sandbox.py
@@ -0,0 +1,63 @@
+import time
+
+from readerdeleter.build.boardsearch import BoardSearch
+from readerdeleter.gaddag import generate_GADDAG
+
+def print_board(b):
+    print('   ' + ''.join(str(i)[-1] for i in range(15)))
+    for i, row in enumerate(b):
+        print(f'{i:<3} ' + ''.join(row))
+
+
+print("reading file")
+t = time.time()
+with open("resources/sample_wordlist_processed.txt", "r") as file:
+    words = [word.strip() for word in file.readlines()]
+print("took", time.time() - t)
+
+
+print("generating gaddag")
+t = time.time()
+gaddag = generate_GADDAG(words)
+print("took", time.time() - t)
+
+board_data = (
+        tuple("               "),
+        tuple("               "),
+        tuple("               "),
+        tuple("               "),
+        tuple("               "),
+        tuple("               "),
+        tuple("   hunched     "),
+        tuple("               "),
+        tuple("               "),
+        tuple("               "),
+        tuple("               "),
+        tuple("               "),
+        tuple("               "),
+        tuple("               "),
+        tuple("               "),
+    )
+
+
+print("generating boardsearch")
+t = time.time()
+board = BoardSearch( board_data, "hurtfuld", gaddag)
+print("took", time.time() - t)
+
+
+print("getting valid words")
+t = time.time()
+valid_words = board.get_valid_words()
+print("took", time.time() - t)
+
+
+print_board(board_data)
+for (direction, r,c,word) in set(valid_words):
+    print(("horizontal", "vertical")[direction], r, c, word)
+    new_board = [[board_data[i][j] for j in range(15)] for i in range(15)]
+    dx = 1 if direction else 0
+    dy = 0 if direction else 1
+    for i in range(len(word)):
+        new_board[r+dx*i][c+dy*i] = word[i]
+    print_board(new_board)
