f0c7776272b2a0775a991edd3a243be00889b886
Daniel <rowedanielp@gmail.com>
2024-11-27 11:37:58 -0800

    feat: scoring

--- dafsa_sandbox.py
+++ dafsa_sandbox.py
@@ -1,92 +1,60 @@
 import time
 
-from readerdeleter.build.boardsearch import BoardSearch
 from readerdeleter.gaddag import generate_GADDAG
+from readerdeleter.board import Board
 
-for _ in range(100000):
-    # TODO: sometimes segfaults--figure out why
-    gaddag = generate_GADDAG(["TEST", "SET", "TESTS"])
-    board_data = (
-            tuple("              T"),
-            tuple("              E"),
-            tuple("              S"),
-            tuple("              T"),
-            tuple("               "),
-            tuple("               "),
-            tuple("               "),
-            tuple("               "),
-            tuple("               "),
-            tuple("               "),
-            tuple("               "),
-            tuple("               "),
-            tuple("               "),
-            tuple("               "),
-            tuple("               "),
-        )
-    board = BoardSearch( board_data, "TES", gaddag)
-    valid_words = list(set(board.get_valid_words()))
-    print(valid_words)
-    assert len(valid_words) == 5
-    assert (1, 0, 14, "....S") in valid_words
-    assert (0, 0, 11, "TES.") in valid_words
-    assert (0, 0, 12, "SE.") in valid_words
-    assert (0, 3, 11, "TES.") in valid_words
-    assert (0, 3, 12, "SE.") in valid_words
 
-#
-# def print_board(b):
-#     print('    ' + ''.join(str(i)[-1] for i in range(15)))
-#     for i, row in enumerate(b):
-#         print(f'{i:<3} ' + ''.join(row))
-#
-#
-# with open("resources/sample_wordlist_processed.txt", "r") as file:
-#     words = [word.strip() for word in file.readlines() if 'z' not in word]
-#
-#
-# print("generating gaddag")
-# t = time.time()
-# gaddag = generate_GADDAG(words)
-# print("took", time.time() - t)
-#
-# board_data = (
-#             tuple("               "),
-#             tuple("               "),
-#             tuple("               "),
-#             tuple("      T        "),
-#             tuple("      E        "),
-#             tuple("      S        "),
-#             tuple("      T        "),
-#             tuple("               "),
-#             tuple("               "),
-#             tuple("               "),
-#             tuple("               "),
-#             tuple("               "),
-#             tuple("               "),
-#             tuple("               "),
-#             tuple("               "),
-#         )
-#
-#
-# print("generating boardsearch")
-# t = time.time()
-# board = BoardSearch( board_data, "SIQRWXA", gaddag)
-# print("took", time.time() - t)
-#
-#
-# print("getting valid words")
-# t = time.time()
-# valid_words = board.get_valid_words()
-# print("got", len(valid_words), "words (", len(set(valid_words)), ") without duplicates")
-# print("took", time.time() - t)
-#
-#
-# print_board(board_data)
-# for (direction, r,c,word) in set(valid_words):
-#     print(("horizontal", "vertical")[direction], r, c, word)
-#     new_board = [[board_data[i][j] for j in range(15)] for i in range(15)]
-#     dx = 1 if direction else 0
-#     dy = 0 if direction else 1
-#     for i in range(len(word)):
-#         new_board[r+dx*i][c+dy*i] = word[i]
-#     print_board(new_board)
+
+with open("resources/sample_wordlist_processed.txt", "r") as file:
+    words = [word.strip() for word in file.readlines() if 'z' not in word]
+# words = ["TEST", "TESTS"]
+
+
+print("generating gaddag")
+t = time.time()
+gaddag = generate_GADDAG(words)
+print("took", time.time() - t)
+
+board_data = [
+            list("               "),
+            list("               "),
+            list("               "),
+            list("               "),
+            list("               "),
+            list("               "),
+            list("               "),
+            list("               "),
+            list("               "),
+            list("               "),
+            list("  BAGS         "),
+            list("               "),
+            list("   T           "),
+            list("               "),
+            list("               "),
+        ]
+
+
+print("generating boardsearch")
+t = time.time()
+board = Board(gaddag, board_data)
+print("took", time.time() - t)
+
+
+print("getting valid words")
+t = time.time()
+plays = board.get_plays("COO")
+print("got", len(plays), "words")
+print("took", time.time() - t)
+
+print("gettings scores")
+t = time.time()
+scores = [board.score_play(play) for play in plays]
+print("took", time.time() - t)
+
+
+print(board)
+for score, play in sorted(zip(scores, plays)):
+    print('\nscore:', score)
+    board_copy = Board(gaddag, [[c for c in row] for row in board_data])
+    board_copy.play(play)
+    print(board_copy)
--- readerdeleter/board.py
+++ readerdeleter/board.py
@@ -3,15 +3,65 @@ from .build.boardsearch import BoardSearch
 
 from itertools import combinations
 
+# TODO:
+# 1. handle start move
+# 2. Figure out features
+#       thinking from https://arxiv.org/pdf/1712.01815
+#       T x 27 planes--one for each letter
+#       where T is history depth (probably 2?)
+#       Also 27 several-hot encoding for rack?
+#       How bad is this gonna be to train? Might have to reduce
+#           something like 27*2*17*17 + 27*12 = ~16,000
+#           so I'd want something on the order of 1 million games?
+#           very doable, but will that work well?
+# 3. Peter thinks Monte Carlo is simply not the way
+#       Actually it might be the way--but start with actual Monte Carlo, not just random
+#       playout
+
+
 # TODO: move this to a better location
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ._"
+SCORES = {
+        "A": 1,
+        "B": 3,
+        "C": 3,
+        "D": 2,
+        "E": 1,
+        "F": 4,
+        "G": 2,
+        "H": 4,
+        "I": 1,
+        "J": 8,
+        "K": 5,
+        "L": 1,
+        "M": 3,
+        "N": 1,
+        "O": 1,
+        "P": 3,
+        "Q": 10,
+        "R": 1,
+        "S": 1,
+        "T": 1,
+        "U": 1,
+        "V": 4,
+        "W": 4,
+        "X": 8,
+        "Y": 4,
+        "Z": 10,
+        "_": 0,
+        }
+
 
 class Board:
-    def __init__(self, gaddag: DAFSA):
+    def __init__(self, gaddag: DAFSA, letters: list[list[str]]|None = None):
         self.size = 15
         self.gaddag = gaddag
-        self.letters = [[" " for _ in range(self.size)] for _ in range(self.size)]
-        self.blanks = []
+        # use capital for letters, lowercase for blanks of that letter
+        if letters is None:
+            self.letters = [[" " for _ in range(self.size)] for _ in range(self.size)]
+        else:
+            self.letters = letters
+        self.blank = [[False for _ in range(self.size)] for _ in range(self.size)]
 
         # lowercase = letter, UPPERCASE = word
         # t = triple, d = double
@@ -39,7 +89,6 @@ class Board:
         """
         generate all permutations of 'word' with 'character' replaced with a blank
         """
-        print("permuting blanks for", word, character)
 
         out = set()
         chunks = word.split(character)
@@ -92,7 +141,6 @@ class Board:
             word_counts = {c: word.count(c) for c in set(word)}
 
             # populate every permutation of necessary blanks
-            print("permuting necessary blanks")
             spare_blanks = rack_counts['_']
             for c, word_count in word_counts.items():
                 if c == '.':
@@ -107,14 +155,11 @@ class Board:
                     for w in words:
                         new_words.update(self.permute_blank_character(w, c, diff))
                     words = new_words
-            print("words looks like", words)
 
             # distribute excess blanks
-            print("distributing excess blanks")
             new_words = set()
             for w in words:
                 new_words.update(self.permute_spare_blank(w, spare_blanks))
-                print("words from", w, "looks like", self.permute_spare_blank(w, spare_blanks))
 
             out.extend([(direction, row, col, word, new_word) for new_word in new_words])
 
@@ -131,3 +176,108 @@ class Board:
         board = BoardSearch(board_data, rack, self.gaddag)
         valid_word_plays = list(set(board.get_valid_words()))
         return self.get_blank_positions(rack, valid_word_plays)
+
+    def auxiliary_score(self, row: int, col: int, char: str, direction: bool):
+        """
+        score the words incidental to a play. For instance, consider the following board:
+
+        t
+        e
+        s
+        t
+        SUPPER
+
+        where "SUPPER" is new.
+        Then we must not only count the score of SUPPER but also that of the new word "tests".
+        """
+        score = 0
+
+        # going one direction
+        cur_r = row + direction
+        cur_c = col + (not direction)
+        while cur_r < self.size and cur_c < self.size and self.letters[cur_r][cur_c] != " ":
+            if not self.blank[cur_r][cur_c]:
+                score += SCORES[self.letters[cur_r][cur_c]]
+            cur_r += direction
+            cur_c += (not direction)
+
+        # going the other direction
+        cur_r = row - direction
+        cur_c = col - (not direction)
+        while cur_r >= 0 and cur_c >= 0 and self.letters[cur_r][cur_c] != " ":
+            if not self.blank[cur_r][cur_c]:
+                score += SCORES[self.letters[cur_r][cur_c]]
+            cur_r -= direction
+            cur_c -= (not direction)
+
+        word_multiplier = 1
+        if score > 0:
+            # a word did exist, so add this letter's score again
+            multiplier = 1
+            if self.score_multipliers[row][col] == "d":
+                multiplier = 2
+            elif self.score_multipliers[row][col] == "t": 
+                multiplier = 3
+            elif self.score_multipliers[row][col] == "D": 
+                word_multiplier *= 2
+            elif self.score_multipliers[row][col] == "T": 
+                word_multiplier *= 3
+            score += SCORES[char] * multiplier
+        return score * word_multiplier
+        
+
+    def score_play(self, play: tuple[int, int, int, str, str]) -> int:
+        """
+        Score a play = (direction, row, col, word, word(with blanks) )
+        """
+        score = 0
+        word_multiplier = 1
+        word = play[4]
+        aux_score = 0
+        for i,char in enumerate(word):
+            row = play[1] + i * (play[0])
+            col = play[2] + i * (not play[0])
+            if char == '.':
+                # preexisting letter
+                if not self.blank[row][col]: # blanks score 0
+                    score += SCORES[self.letters[row][col]]
+            else:
+                multiplier = 1
+                if self.score_multipliers[row][col] == "d":
+                    multiplier = 2
+                elif self.score_multipliers[row][col] == "t": 
+                    multiplier = 3
+                elif self.score_multipliers[row][col] == "D": 
+                    word_multiplier *= 2
+                elif self.score_multipliers[row][col] == "T": 
+                    word_multiplier *= 3
+                score += SCORES[char] * multiplier
+                aux_score += self.auxiliary_score(row, col, char, not play[0])
+        return score * word_multiplier + aux_score
+
+    def play(self, play: tuple[int, int, int, str, str]) -> None:
+        column_word, row, col, word, word_blanks = play
+        for i,char in enumerate(word):
+            cur_r = row + i * (column_word)
+            cur_c = col + i * (not column_word)
+            if char != ".":
+                self.letters[cur_r][cur_c] = char
+                if word_blanks[i] == '_':
+                    self.blank[cur_r][cur_c] = True
+
+
+
+
+    def __str__(self) -> str:
+        out = '    ' + ''.join(str(i)[-1] for i in range(15))
+        out += '\n'
+        for i, row in enumerate(self.letters):
+            out += f'{i:<3} '
+            for j, c in enumerate(row):
+                if c == ' ':
+                    out += self.score_multipliers[i][j]
+                    # out += ' '
+                else:
+                    out += c
+            out += "\n"
+        return out
